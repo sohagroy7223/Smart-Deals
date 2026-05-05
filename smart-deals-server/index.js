625b43d4f44f4b886feacaab78fe931a4b1392d6
sohagroy7223 <freelancersohagroy@gmail.com>
2026-05-05 17:47:52 +0600

    addad delete method and convert all bids products to array

--- smart-deals-server/index.js
+++ smart-deals-server/index.js
@@ -110,6 +110,19 @@ async function run() {
       res.send(result);
     });
 
+    app.get("bids", async (req, res) => {
+      const cursor = bidsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.delete("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bidsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
