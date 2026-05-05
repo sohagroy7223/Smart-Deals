f644a39ae22b881aed5adb87a9fcc135d4d6850b
sohagroy7223 <freelancersohagroy@gmail.com>
2026-05-06 01:43:46 +0600

    addad remove the tailwind v4 and install v3 and solve the problem

--- smart-deals-client/src/App.jsx
+++ smart-deals-client/src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h2 className="text-green-700 text-center">Get started</h2>
+      <h1>Get started</h1>
     </>
   );
 }
--- smart-deals-client/src/main.jsx
+++ smart-deals-client/src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+// import App from "./App.jsx";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from "./Components/Layout/Root.jsx";
+import Home from "./Components/Home/Home.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
