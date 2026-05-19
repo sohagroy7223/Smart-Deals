724cc8b890da9846db1f7177519104eb64e7b4e9
sohagroy7223 <freelancersohagroy@gmail.com>
2026-05-19 22:11:22 +0600

    addad products details

--- smart-deals-client/src/Components/Page/LatestProducts.jsx
+++ smart-deals-client/src/Components/Page/LatestProducts.jsx
@@ -5,13 +5,15 @@ const LatestProducts = ({ latestProductsPromise }) => {
   const products = use(latestProductsPromise);
   console.log(products);
   return (
-    <div className="text-center mt-5">
+    <div className="text-center mt-5 ">
       <h3 className="text-3xl font-bold">
         Latest <span className="text-primary">Products</span>
+      </h3>
+      <div className="gap-3 rounded-2xl grid md:grid-cols-2 lg:grid-cols-3">
         {products.map((product) => (
           <Product product={product}></Product>
         ))}
-      </h3>
+      </div>
     </div>
   );
 };
--- smart-deals-client/src/Components/Product/Product.jsx
+++ smart-deals-client/src/Components/Product/Product.jsx
@@ -1,8 +1,19 @@
 const Product = ({ product }) => {
   console.log(product);
+  const { title, price_min, price_max, usage, image } = product;
   return (
-    <div>
-      <h3></h3>
+    <div className="border text-start p-1 bg-gray-50 rounded-lg">
+      <img
+        className="h-60 max-w-full mx-auto p-3 rounded-2xl bg-cover"
+        src={image}
+        alt=""
+      />
+      <h3 className=" text-xl font-bold">
+        {title}-[{usage}]
+      </h3>
+      <p className="font-medium">
+        price:{price_min}-{price_max}
+      </p>
     </div>
   );
 };
