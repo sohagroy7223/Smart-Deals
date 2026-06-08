cd55eddba5987862c522b0caccfc7c721cc219c5
sohagroy7223 <freelancersohagroy@gmail.com>
2026-06-08 15:34:48 +0600

    addad env in client side

--- smart-deals-client/src/Firebase/Firebase.init.js
+++ smart-deals-client/src/Firebase/Firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCUe-LgLns25N-aQ42-iLWs3VqpDdNY_3k",
-  authDomain: "smart-deals-26625.firebaseapp.com",
-  projectId: "smart-deals-26625",
-  storageBucket: "smart-deals-26625.firebasestorage.app",
-  messagingSenderId: "78166271193",
-  appId: "1:78166271193:web:74217b6d161ac28d204ac8",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
